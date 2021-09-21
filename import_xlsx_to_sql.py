9b248ccfa60994fe1cb58f32d5b4e2ae67d507c3
kidclone3 <kid.clone.3@gmail.com>
2021-09-22 00:04:56 +0700

    First

---
+++ import_xlsx_to_sql.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from pandas.core.indexing import _iLocIndexer
+df = pd.read_excel('dean.xlsx')
+
+# print(df)
+print('INSERT INTO DEAN(TENDA,MADA,DDIEM_DA,PHONG) VALUES ', end = ' ')
+for i in range(len(df)):
+    print('(', end='')    
+    for j in range(len(df.iloc[i])):
+        if (j != len(df.iloc[i])-1):
+            ed = ', '
+        else:
+            ed = ' '
+        print(f'\'{df.iloc[i, j]}\'', end = ed)
+    print(end = '),')
+# print(df.iloc[0, 0])
